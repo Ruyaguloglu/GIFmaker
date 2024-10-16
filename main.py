9a189e920879ff3618a52bcf6e4ae7deee680961
Rüya Güloğlu <gulogluruya2@gmail.com>
2024-10-16 17:02:57 +0300

    file added

---
+++ main.py
@@ -0,0 +1,16 @@
+import imageio.v3 as iio
+
+filenames = ['pic1.png', 'pic2.png']
+images = [ ]
+
+for filename in filenames:
+  images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration = 500, loop = 0)
+
+#'team.gif': This is the name you want to give to your new GIF file.
+#images: The list containing the image data.
+#duration = 500: How long each picture should show in the GIF, in milliseconds.
+#loop = 0: How many times the GIF should repeat (0 means it keeps looping forever).
+
+
